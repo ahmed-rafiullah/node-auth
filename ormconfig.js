229dae929861a61e43d509c78ad9f58bd7abc415
Ahmed Rafiullah <khattak.ahmed@yahoo.com>
2020-07-07 12:18:26 +0500

    added typeorm config

---
+++ ormconfig.js
@@ -0,0 +1,34 @@
+
+
+const {
+    DATABASE_HOST = 'localhost',
+    DATABASE_USER = 'root',
+    DATABASE_PASSWORD = '12345',
+    DATABASE_NAME = 'simple'
+
+} = process.env
+
+
+
+
+
+module.exports = 
+{
+    type: "mysql",
+    host: DATABASE_HOST,
+    port: 4050,
+    username: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    database: DATABASE_NAME,
+    synchronize: true,
+    logging: false,
+    migrationsTableName: 'migrations',
+    entities: [
+        "src/entities/**/*.ts"
+     ],
+     migrations: [
+        "src/migrations/**/*.ts"
+     ],
+
+    
+}
