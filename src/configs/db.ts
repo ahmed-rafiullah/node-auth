db741da79fb8a074c7ce9084085ef2b918ff3522
Ahmed Rafiullah <khattak.ahmed@yahoo.com>
2020-07-14 18:23:59 +0500

    Fixed default db user name

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,6 +1,6 @@
 export const {
     DATABASE_HOST = 'localhost',
-    DATABASE_USER = 'rootyy',
+    DATABASE_USER = 'root',
     DATABASE_PASSWORD = 'example',
     DATABASE_NAME = 'simple',
     DATABASE_PORT = 4050
